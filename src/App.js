a56d1fa42167b8b58bd74bcd2edc636c68ec5079
Duoc Nguyen C <satthukt1232@gmail.com>
2017-10-20 10:06:49 +0700

    Understand the React Component Lifecycle Methods

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import PropTypes from 'prop-types';
-import './App.css';
+import ReactDOM from 'react-dom';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {val: 0}
+    this.update = this.update.bind(this)
+  }
+  update() {
+    this.setState({val: this.state.val + 1})
+  }
+  componentWillMount() {
+    console.log('componentWillMount');
+  }
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+  render() {
+    console.log('render');
+    return (
+      <button onClick={this.update}>{this.state.val}</button> 
+    );
+  }
+  componentWillUnMount() {
+    console.log('componentWillUnMount');
+  }
+}
+
+class Wrapper extends Component {
+  mount() {
+    ReactDOM.render(<App />, document.getElementById('a'))
+  }
+  unmount() {
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unmount.bind(this)}>Unmount</button>
       </div>
     );
   }
 }
 
-export default App;
+export default Wrapper;
